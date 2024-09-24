7e6f41324f3ec9e2439708191afd285b950ffc91
elnai534 <elinachin513@gmail.com>
2024-09-24 17:31:40 -0600

    Initial commit

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <div>
+      <main>
+        <h1>Shopping List</h1>
+      </main>
+      <StudentInfo />
+    </div>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Elina Chin</h1>
+      <Link href="https://github.com/elnai534/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+        GitHub Repository
+      </Link>
+    </div>
+  );
+}
+
+export default StudentInfo
